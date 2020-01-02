0bede56d8ad0df3a3fde76d557e4fe75f0aee05b
NandoCM <nando@tipit.net>
2020-01-02 16:27:53 -0600

    atxitalianchallenge badge

---
+++ src/badge_templates/atxitalianchallenge/Tasting Card/35x55/badge_1.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Textfit } from 'react-textfit';
+
+import './styles/styles_1.less';
+import background_img from './images/background_1.png';
+
+
+export default ({badge}) => {
+    return (
+    <>
+        <div id="badge-artboard" className="bdg-artboard">
+            <img id="badge-artboard-img" className="bdg-image" src={background_img}/>
+
+            <div id="name-on-badge" className="bdg-content text-box text-box-1">
+                <Textfit mode="single" max={35} className="box-content">{badge.getFullName()}</Textfit>
+            </div>
+
+            {badge.hasQRCode() &&
+            <div id="qrcode" className="bdg-content qrcode-box">
+                {badge.getQRCode()}
+            </div>
+            }
+
+            {badge.getFeature('VIP Access') &&
+            <div id="icon-feature-1" className="bdg-content icon-feature">
+                <img className="bdg-image" src={feature_1_img}/>
+            </div>
+            }
+
+        </div>
+    </>
+    );
+}
+
