d09f457bb2770970440b99c4431b4160f5f65fe8
NandoCM <17532550+NandoJourneyman@users.noreply.github.com>
2020-02-12 12:21:00 -0600

    directory, General badge logic

--- src/badge_templates/mock-event/General Admission/badge_1.js
+++ src/badge_templates/mock-event/General Admission/badge_1.js
@@ -4,8 +4,8 @@ import { Textfit } from 'react-textfit';
 import './styles/styles_1.less';
 import background_img from './images/background_1.png';
 import feature_1_img from './images/feature_1.png';
-import feature_1_img from './images/feature_2.png';
-import feature_1_img from './images/feature_3.png';
+import feature_2_img from './images/feature_2.png';
+import feature_3_img from './images/feature_3.png';
 
 export default ({badge}) => {
     return (
@@ -26,9 +26,21 @@ export default ({badge}) => {
             </div>
             }
 
-            {badge.getFeature('VIP Access') &&
-            <div id="icon-feature-1" className="bdg-content icon-feature">
+            {badge.getFeature('Expo') &&
+            <div id="icon-feature" className="bdg-content icon-feature">
                 <img className="bdg-image" src={feature_1_img}/>
+            </div>
+            }
+
+            {badge.getFeature('Media') &&
+            <div id="icon-feature" className="bdg-content icon-feature">
+                <img className="bdg-image" src={feature_2_img}/>
+            </div>
+            }
+
+            {badge.getFeature('Speaker') &&
+            <div id="icon-feature" className="bdg-content icon-feature">
+                <img className="bdg-image" src={feature_3_img}/>
             </div>
             }
 
