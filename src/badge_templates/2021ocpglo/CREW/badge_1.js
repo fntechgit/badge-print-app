f09e6a4a483d97cc321905c7767b8e686d9ef074
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2021-10-29 10:08:55 -0300

    Adds IF Staff and Crew badge templates. Adds OCP Crew badge template.

---
+++ src/badge_templates/2021ocpglo/CREW/badge_1.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Textfit } from 'react-textfit';
+
+import './styles/styles_1.less';
+
+import background_img from './images/background.png';
+
+
+function useForceUpdate(){
+    const [value, setValue] = useState(0);
+    console.log("forceUpdate")
+    return () => setValue(value => value + 1);
+}
+
+export default ({badge}) => {
+
+    const forceUpdate = useForceUpdate();
+
+    return (
+    <>
+        <div id="badge-artboard" className="bdg-artboard">
+            <img id="badge-artboard-img" className="bdg-image" src={background_img}/>
+            <div className="text-box">
+                <Textfit mode="single" max={48} className="first-name" onInput={forceUpdate} contentEditable suppressContentEditableWarning={true}>{badge.getFirstName()}</Textfit>
+                <Textfit mode="single" max={35} className="last-name" onInput={forceUpdate} contentEditable suppressContentEditableWarning={true}>{badge.getLastName()}</Textfit>
+            </div>
+        </div>
+    </>
+    );
+}
+
---
+++ src/badge_templates/infinityfestival2021/STAFF/badge_1.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Textfit } from 'react-textfit';
+
+import './styles/styles_1.less';
+
+import background_img from './images/background.png';
+
+
+function useForceUpdate(){
+    const [value, setValue] = useState(0);
+    return () => setValue(value => value + 1);
+}
+
+export default ({badge}) => {
+
+    const forceUpdate = useForceUpdate();
+
+    return (
+    <>
+        <div id="badge-artboard" className="bdg-artboard">
+            <img id="badge-artboard-img" className="bdg-image" src={background_img}/>
+            <div className="text-box">
+                <Textfit mode="single" max={40} className="first-name" onInput={forceUpdate} contentEditable suppressContentEditableWarning={true}>{badge.getFirstName()}</Textfit>
+                <Textfit mode="single" max={40} className="last-name" onInput={forceUpdate} contentEditable suppressContentEditableWarning={true}>{badge.getLastName()}</Textfit>
+                <Textfit mode="single" max={24} className="company"  onInput={forceUpdate} contentEditable suppressContentEditableWarning={true}>{badge.getCompany()}</Textfit>
+            </div>
+            {badge.hasQRCode() &&
+            <div id="qrcode" className="bdg-content qrcode-box">
+                {badge.getQRCode({ fgColor: '#000', size: 80 })}
+            </div>
+            }
+        </div>
+    </>
+    );
+}
+
