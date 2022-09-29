e5bc76bf89f6f5a256fad6063ae74756e0fd055b
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2022-09-29 11:46:30 -0300

    Adds OCP 2022 badge templates.

---
+++ src/badge_templates/2022ocpglo/CREW/Card.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { Textfit } from 'react-textfit';
+
+import {
+    useForceUpdate
+} from '@/utils/utils';
+
+import './styles/styles_1.less';
+
+import background_img from './images/crew94x140.svg';
+
+export default ({badge}) => {
+    const forceUpdate = useForceUpdate();
+    return (
+    <>
+        <div id="badge-artboard" className="bdg-artboard">
+            <img id="badge-artboard-img" className="bdg-image" src={background_img}/>
+            <div className="content-wrapper">
+                <Textfit mode="single" max={42} className="first-name" onInput={forceUpdate} contentEditable suppressContentEditableWarning={true}>{badge.getFirstName()}</Textfit>
+                <Textfit mode="single" max={42} className="last-name" onInput={forceUpdate} contentEditable suppressContentEditableWarning={true}>{badge.getLastName()}</Textfit>
+            </div>
+        </div>
+    </>
+    );
+}
+
