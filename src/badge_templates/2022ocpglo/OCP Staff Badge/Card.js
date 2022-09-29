3ceecf2dc12898dbe9ab711197fdc92150618daf
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2022-09-29 11:46:30 -0300

    Adds OCP 2022 badge templates.

---
+++ src/badge_templates/2022ocpglo/OCP Staff Badge/Card.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Textfit } from 'react-textfit';
+
+import {
+    useForceUpdate
+} from '@/utils/utils';
+
+import './styles/styles_1.less';
+
+import background_img from './images/staff94x140.svg';
+import logo from './images/hex-logo.svg';
+
+export default ({badge}) => {
+    const forceUpdate = useForceUpdate();
+    return (
+    <>
+        <div id="badge-artboard" className="bdg-artboard">
+            <img id="badge-artboard-img" className="bdg-image" src={background_img}/>
+            <div className="text-box">
+                <Textfit mode="single" max={42} className="first-name" onInput={forceUpdate} contentEditable suppressContentEditableWarning={true}>{badge.getFirstName()}</Textfit>
+                <Textfit mode="single" max={42} className="last-name" onInput={forceUpdate} contentEditable suppressContentEditableWarning={true}>{badge.getLastName()}</Textfit>
+                <span className="badge-title ocp-staff">OCP Staff</span>
+                <span className="attendee-title" contentEditable>{badge.getExtraQuestionValue('Job Title')}</span>
+            </div>
+            <div className="bdg-content hex-logo">
+                <img className="bdg-image" src={logo}/>
+            </div>
+        </div>
+    </>
+    );
+}
