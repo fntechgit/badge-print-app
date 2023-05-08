06d421b1396f8015fe143bd33e7060b345650dda
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2023-05-08 18:17:10 -0300

    Adds CampStrava 2023 Crew badge templates.

---
+++ src/badge_templates/campstrava2023/CREW Badge/ZebraCard.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { Textfit } from 'react-textfit';
+
+import {
+    useForceUpdate
+} from '@/utils/utils';
+
+import './styles/zebra-card.less';
+
+import background_img from './images/crew89x142.2.svg';
+
+export default ({badge}) => {
+    const forceUpdate = useForceUpdate();
+    return (
+    <>
+        <div id="badge-artboard" className="bdg-artboard">
+            <img className="bdg-image" src={background_img}/>
+            <div className="content-wrapper">
+                <Textfit mode="single" max={42} className="first-name" onInput={forceUpdate} contentEditable suppressContentEditableWarning={true}>{badge.getFirstName()}</Textfit>
+                <Textfit mode="single" max={42} className="last-name" onInput={forceUpdate} contentEditable suppressContentEditableWarning={true}>{badge.getLastName()}</Textfit>
+            </div>
+        </div>
+    </>
+    );
+}
+
