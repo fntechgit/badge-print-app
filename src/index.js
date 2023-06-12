e9bdd0df0e99a90d5d79958694b55387165cee51
smarcet <smarcet@gmail.com>
2023-06-12 20:43:55 -0300

    Added new marketing setting variable per summit * site_font

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ import { Provider } from 'react-redux'
 import store, {persistor} from './store';
 import { PersistGate } from 'redux-persist/es/integration/react'
 import './styles/colors.scss';
+import './styles/fonts.scss'
 import 'font-awesome/css/font-awesome.css';
 import './styles/general.less';
 import 'sweetalert2/dist/sweetalert2.css';
