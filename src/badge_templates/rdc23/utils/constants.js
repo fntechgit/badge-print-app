1ccce57e82b3c7af2a093268d369754243fad7eb
Zachary Nespral <zachary.nespral@fntech.com>
2023-07-20 12:46:15 -0700

    Updated colors for wristband badge

--- src/badge_templates/rdc23/utils/constants.js
+++ src/badge_templates/rdc23/utils/constants.js
@@ -52,15 +52,15 @@ export const ZebraCardBadgeTypesColor = {
 export const WristbandBadgeTypesColor = {
     // elected to match Pantone Black 6 C - Page C 189
     [BadgeTypes.Staff]: '#100420',
-    // elected to match Pantone 2995 C - Page C 107
-    [BadgeTypes.Volunteer]: '#00A9E0',
+    // elected to match Pantone 2985 C
+    [BadgeTypes.Volunteer]: '#54C0E8',
     [BadgeTypes.Attendee]: '#FFFFFF',
-    // elected to match Pantone 2995 C - Page C 107
-    [BadgeTypes.Speaker]: '#00A9E0',
-    // elected to match Pantone 2725 C - Page C 82
-    [BadgeTypes.ExternalSpeaker]: '#685BC7',
-    // elected to match Pantone 7488 C - Page C 143
-    [BadgeTypes.Press]: '#78D64B',
-    // elected to match Pantone 2725 C - Page C 82
-    [BadgeTypes.Guest]: '#685BC7',
+    // elected to match Pantone 2985 C
+    [BadgeTypes.Speaker]: '#54C0E8',
+    // elected to match Pantone 272 C
+    [BadgeTypes.ExternalSpeaker]: '#7473C0',
+    // elected to match Pantone 7487 C
+    [BadgeTypes.Press]: '#89DC65',
+    // elected to match Pantone 272 C
+    [BadgeTypes.Guest]: '#7473C0',
 };
