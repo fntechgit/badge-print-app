74847e8b817fbb431e73fe41a31e0dfe38726e4e
Zachary Nespral <zachary.nespral@fntech.com>
2023-07-20 13:24:37 -0700

    Updated color comments for reference

--- src/badge_templates/rdc23/utils/constants.js
+++ src/badge_templates/rdc23/utils/constants.js
@@ -52,15 +52,15 @@ export const ZebraCardBadgeTypesColor = {
 export const WristbandBadgeTypesColor = {
     // elected to match Pantone Black 6 C - Page C 189
     [BadgeTypes.Staff]: '#100420',
-    // elected to match Pantone 2985 C - AKA Blue Speaker
+    // elected to match Pantone 2975 C - AKA Blue Speaker
     [BadgeTypes.Volunteer]: '#95D4E9',
     [BadgeTypes.Attendee]: '#FFFFFF',
-    // elected to match Pantone 2985 C - AKA Blue Speaker
+    // elected to match Pantone 2975 C - AKA Blue Speaker
     [BadgeTypes.Speaker]: '#95D4E9',
-    // elected to match Pantone 272 C - AKA Purple Speaker
+    // elected to match Pantone 2735 C - AKA Purple Speaker
     [BadgeTypes.ExternalSpeaker]: '#270089',
-    // elected to match Pantone 7487 C - AKA Green Media
+    // elected to match Pantone 374 C - AKA Green Media
     [BadgeTypes.Press]: '#C2E76B',
-    // elected to match Pantone 272 C - AKA Purple Speaker
+    // elected to match Pantone 2735 C - AKA Purple Speaker
     [BadgeTypes.Guest]: '#270089',
 };
