1f0a2795f30440713aee74839a7512b15da69143
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2024-05-31 23:19:18 -0300

    Implements text display changes based on number of lines.

--- src/badge_templates/sff2024/Card.js
+++ src/badge_templates/sff2024/Card.js
@@ -1,100 +1,194 @@
-import React, {
-  useRef,
-  useState,
-  useEffect,
-  useCallback
-} from "react";
+import React, { useRef, useState, useEffect, useCallback } from "react";
 import { Textfit } from "react-textfit";
-
 import background_img from "./images/background.png";
 import "./styles/styles_1.less";
+
+const calculateNumberOfLines = (ref) => {
+  if (!ref.current) return 0;
+  const rect = ref.current.getBoundingClientRect();
+  const lineHeight = parseInt(getComputedStyle(ref.current).lineHeight);
+  return Math.floor(ref.current.clientHeight / lineHeight);
+};
 
 const useForceUpdate = () => {
   const [value, setValue] = useState(0);
   return () => setValue(value => value + 1);
 }
 
-export default ({ badge }) => {
-  const forceUpdate = useForceUpdate();
-  const companyRef = useRef(null);
-  const [isCompanyMultiLine, setIsCompanyMultiLine] = useState(false);
+const FullName = ({ badge, fullNameLines, setFullNameLines, forceUpdate }) => {
+  const fullNameRef = useRef(null);
+
   useEffect(() => {
-    if (companyRef.current === null) return false;
-    setIsCompanyMultiLine(companyRef.current.clientHeight > 26);
-  }, [companyRef]);
-  const companyMarginTop = useCallback(() => {
-    if (!isCompanyMultiLine) return "2.3mm";
-    if (isCompanyMultiLine) return "10.8mm";
-    return 0;
-  }, [isCompanyMultiLine]);
-  const companyHeight = useCallback(() => isCompanyMultiLine ? "75px" : "38px", [isCompanyMultiLine])
+    if (fullNameRef.current) {
+      setFullNameLines(calculateNumberOfLines(fullNameRef));
+    }
+  }, [fullNameRef, setFullNameLines]);
+
   return (
-  <>
-    <div id="badge-artboard" className="bdg-artboard">
-      <img id="badge-artboard-img" className="bdg-image" src={background_img}/>
-      <div className="text-boxes">
-        {badge.getFullName() && 
+    <>
+      {badge.getFullName() && fullNameLines === 1 && (
+        <span
+          ref={fullNameRef}
+          className="full-name"
+          contentEditable
+          suppressContentEditableWarning={true}
+          onInput={() => setFullNameLines(calculateNumberOfLines(fullNameRef))}
+        >
+          {badge.getFullName()}
+        </span>
+      )}
+      {fullNameLines > 1 && badge.getFirstName() && (
+        <Textfit
+          mode="single"
+          max={32}
+          className="name"
+          contentEditable
+          suppressContentEditableWarning={true}
+          onInput={forceUpdate}
+        >
+          {badge.getFirstName()}
+        </Textfit>
+      )}
+      {fullNameLines > 1 && badge.getLastName() && (
+        <Textfit
+          mode="single"
+          max={32}
+          className="name"
+          contentEditable
+          suppressContentEditableWarning={true}
+          onInput={forceUpdate}
+        >
+          {badge.getLastName()}
+        </Textfit>
+      )}
+    </>
+  );
+};
+
+const Company = ({
+  badge,
+  company,
+  companyLines,
+  setCompanyLines,
+  companyMarginTop,
+  setCompany,
+  forceUpdate
+}) => {
+  const companyRef = useRef(null);
+  const [companyLineHeight, setCompanyLineHeight] = useState();
+
+  useEffect(() => {
+    if (companyRef.current) {
+      setCompanyLines(calculateNumberOfLines(companyRef));
+      setCompanyLineHeight(parseInt(getComputedStyle(companyRef.current).lineHeight));
+    }
+  }, [companyRef, setCompanyLines, setCompanyLineHeight]);
+
+  const handleCompanyInput = (event) => {
+    setCompany(event.target.innerText);
+    setCompanyLines(calculateNumberOfLines(companyRef));
+  };
+
+  return (
+    <>
+      {companyLines <= 2 && (
+        <span
+          ref={companyRef}
+          className="company"
+          contentEditable
+          suppressContentEditableWarning={true}
+          onInput={handleCompanyInput}
+          style={{
+            marginTop: companyMarginTop(),
+            fontSize: "20pt",
+          }}
+        >
+          {console.log("rendering span")}
+          {badge.getCompany()}
+        </span>
+      )}
+      {companyLines > 2 && (
+        <Textfit
+          mode="multi"
+          max={24.5}
+          className="company"
+          contentEditable
+          suppressContentEditableWarning={true}
+          onInput={forceUpdate}
+          style={{
+            marginTop: companyMarginTop(),
+            height: companyLineHeight * 2,
+          }}
+        >
+          {console.log("rendering textfit")}
+          {company}
+        </Textfit>
+      )}
+    </>
+  );
+};
+
+const Badge = ({ badge }) => {
+  const forceUpdate = useForceUpdate();
+  const [fullNameLines, setFullNameLines] = useState(1);
+  const [companyLines, setCompanyLines] = useState(1);
+  const [company, setCompany] = useState(badge.getCompany());
+
+  const companyMarginTop = useCallback(() => {
+    if (fullNameLines === 1 && companyLines <= 1) return "16.3mm";
+    if (fullNameLines === 1 && companyLines > 1) return "10.8mm";
+    if (fullNameLines > 1 && companyLines <= 1) return "8.3mm";
+    if (fullNameLines > 1 && companyLines > 1) return "3.3mm";
+    return 0;
+  }, [fullNameLines, companyLines]);
+
+  return (
+    <>
+      <div id="badge-artboard" className="bdg-artboard">
+        <img id="badge-artboard-img" className="bdg-image" src={background_img} alt="Background" />
+        <div className="text-boxes">
+          <FullName
+            badge={badge}
+            fullNameLines={fullNameLines}
+            setFullNameLines={setFullNameLines}
+            forceUpdate={forceUpdate}
+          />
+          <Company
+            badge={badge}
+            company={company}
+            setCompany={setCompany}
+            companyLines={companyLines}
+            setCompanyLines={setCompanyLines}
+            companyMarginTop={companyMarginTop}
+            forceUpdate={forceUpdate}
+          />
+          {badge.getExtraQuestionValue("jobTitle") && (
           <Textfit
-            max={32}
-            className="full-name"
-            onInput={forceUpdate}
-            contentEditable
-            suppressContentEditableWarning={true}
-            style={{
-              height: "75px"
-            }}
-          >
-            {badge.getFullName()} 
-          </Textfit>
-        }
-        <div style={{ position: "relative", display: "flex"}}>
-          {badge.getCompany() &&
-            <Textfit
-              max={24}
-              min={22}
-              mode="multi"
-              className="company"
-              onInput={forceUpdate}
-              contentEditable
-              suppressContentEditableWarning={true}
-              style={{
-                // width: "282px",
-                // maxHeight: "75px",
-                // height: "75px",
-                display: "flex"
-              }}
-            >
-              <span ref={companyRef}>{badge.getCompany()}</span>
-            </Textfit>
-          }
-        </div>
-        {badge.getExtraQuestionValue("jobTitle") &&
-          <Textfit
-            max={17}
+            min={12}
+            max={18}
             className="job-title"
-            onInput={forceUpdate}
             contentEditable
             suppressContentEditableWarning={true}
-            style={{
-              height: "35px"
-            }}
+            onInput={forceUpdate}
+            style={{ height: 20 }}
           >
             {badge.getExtraQuestionValue("jobTitle")}
           </Textfit>
-        }
+          )}
+        </div>
+        {(badge.getFeature("Booth Staff") || badge.getFeature("Partner Booth")) && (
+        <Textfit
+          min={26}
+          className="booth-staff-feature"
+          contentEditable
+          suppressContentEditableWarning={true}
+        >
+          Partner Booth
+        </Textfit>
+        )}
       </div>
-      {(badge.getFeature("Booth Staff") || badge.getFeature("Partner Booth")) &&
-      <Textfit
-        min={26}
-        className="booth-staff-feature"
-        onInput={forceUpdate}
-        contentEditable
-        suppressContentEditableWarning={true}
-      >
-        Partner Booth
-      </Textfit>
-      }
-    </div>
-  </>
+    </>
   );
-}
+};
+
+export default Badge;
