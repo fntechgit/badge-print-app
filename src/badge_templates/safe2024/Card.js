4dd5b97ccfb41594dc8b9a2f9db525333391f900
Sam Young <sam.young@fntech.com>
2024-06-02 12:22:20 -0700

    text displays and positioning

--- src/badge_templates/safe2024/Card.js
+++ src/badge_templates/safe2024/Card.js
@@ -17,35 +17,77 @@ const useForceUpdate = () => {
 
 export default ({ badge }) => {
   const forceUpdate = useForceUpdate();
+  const fullNameRef = useRef(null);
+  const companyRef = useRef(null);
+  const [isFullNameMultiLine, setIsFullNameMultiLine] = useState(false);
+  const [isCompanyMultiLine, setIsCompanyMultiLine] = useState(false);
+  useEffect(() => {
+    if (fullNameRef.current === null) return false;
+    setIsFullNameMultiLine(fullNameRef.current.clientHeight > 46);
+  }, [fullNameRef]);
+  useEffect(() => {
+    if (companyRef.current === null) return false;
+    setIsCompanyMultiLine(companyRef.current.clientHeight > 42);
+  }, [companyRef]);
+  const companyMarginTop = useCallback(() => {
+    if (!isFullNameMultiLine && !isCompanyMultiLine) return "5.3mm";
+    if (!isFullNameMultiLine && isCompanyMultiLine) return "10.8mm";
+    if (isFullNameMultiLine && !isCompanyMultiLine) return "2.3mm";
+    if (isFullNameMultiLine && isCompanyMultiLine) return "3.3mm";
+    return 0;
+  }, [isFullNameMultiLine, isCompanyMultiLine]);
   return (
   <>
     <div id="badge-artboard" className="bdg-artboard">
       <img id="badge-artboard-img" className="bdg-image" src={background_img}/>
       <div className="text-boxes">
-        <div style={{ height: "70px"}}>
-        {badge.getFullName() && 
-          <Textfit
-            max={32}
+          {badge.getFullName() && !isFullNameMultiLine &&
+          <span
+            ref={fullNameRef}
             className="full-name"
+            contentEditable
+            suppressContentEditableWarning={true}
+            onInput={() => setIsFullNameMultiLine(fullNameRef.current.clientHeight > 46)}
+          >
+            {badge.getFullName()}
+          </span>
+          }
+          {isFullNameMultiLine && badge.getFirstName() &&
+          <Textfit
+            mode="single"
+            max={32}
+            className="name"
             onInput={forceUpdate}
             contentEditable
             suppressContentEditableWarning={true}
-            style={{
-              height: "70px"
-            }}
           >
-            {badge.getFullName()}
+            {badge.getFirstName()}
           </Textfit>
-        }
-        </div>
+          }
+          {isFullNameMultiLine && badge.getLastName() &&
+          <Textfit
+            mode="single"
+            max={32}
+            className="name"
+            onInput={forceUpdate}
+            contentEditable
+            suppressContentEditableWarning={true}
+          >
+            {badge.getLastName()}
+          </Textfit>
+          }
         {badge.getCompany() &&
         <Textfit
           max={24}
           mode="single"
           className="company"
+          ref={companyRef}
           onInput={forceUpdate}
           contentEditable
           suppressContentEditableWarning={true}
+          style={{
+            marginTop: companyMarginTop()
+          }}
         >
           {badge.getCompany()}
         </Textfit>
